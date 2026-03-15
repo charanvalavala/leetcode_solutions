8d00e8251d9a0bca4964cc61b528f443f55cd9eb
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-15 12:21:09 +0530

    Time: 44 ms (45.32%), Space: 220.3 MB (34.48%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,28 +1,21 @@
 class MyHashMap {
-    int arr[1000001];
+private:
+    int data[1000001]; 
+
 public:
     MyHashMap() {
-        fill(arr, arr + 1000001, -1);
+        fill(data, data + 1000001, -1);
     }
     
     void put(int key, int value) {
-        arr[key]= value;
+        data[key] = value;
     }
     
     int get(int key) {
-        return arr[key];
+        return data[key];
     }
     
     void remove(int key) {
-        arr[key] = -1;
-        
+        data[key] = -1;
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
