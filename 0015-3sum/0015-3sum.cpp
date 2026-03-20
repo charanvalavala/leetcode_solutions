e2cee67507cdd5717a667ad2e8e77c0a53470a59
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-20 19:47:48 +0530

    Time: 0 ms (100%), Space: 28.9 MB (99.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,61 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
         int n = nums.size();
-
-    vector<vector<int>> ans;
-
-    sort(nums.begin(), nums.end());
-    for (int i = 0; i < n; i++)
-    {
-
-        if (i > 0 && nums[i] == nums[i - 1])
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
         {
-            continue;
-        }
-
-        int l = i + 1;
-        int r = n - 1;
-
-        while (l < r)
-        {
-            int sum = nums[i] + nums[l] + nums[r];
-
-            if (sum == 0)
+            if(i>0 && nums[i] == nums[i-1])continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k)
             {
-                ans.push_back({nums[i], nums[l], nums[r]});
-
-
-         while(l<r && nums[l] == nums[l+1]){
-            l++;
-         }
-
-         while ( l < r && nums[r] == nums[r-1]){
-            r--;
-         }
-
-         l++;
-         r--;
-            }
-
-            else if (sum < 0)
-            {
-                l++;
-            }
-
-            else
-            {
-                r--;
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
+                    j++;
+                }
+                else if(sum >0)
+                {
+                    k--;
+                }
+                else
+                {
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
             }
         }
-
-        
-    }
-
-    return ans ;
+        return ans;
     }
 };
-
 auto __fast_io_atexit = []() {
         ios::sync_with_stdio(false);
             cin.tie(nullptr);
