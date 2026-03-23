883d7ac29ec42ba5896805c9a82268235dadda02
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-23 22:38:50 +0530

    Time: 569 ms (15.8%), Space: 243.5 MB (15.24%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        return mergesort(nums,0,n-1);
+    }
+
+    int mergesort(vector<int>& nums,int low,int high)
+    {
+        int count =0;
+        if(low >=high)return count;
+        int mid =(low+high)/2;
+        count += mergesort(nums,low,mid);
+        count += mergesort(nums,mid+1,high);
+        count += merge(nums,low,mid,high);
+        return count;
+    }
+
+    int merge(vector<int>& nums,int low,int mid,int high)
+    {
+        int count=0;
+        vector<int>tmp;
+        int left = low;
+        int right = mid+1;
+        while(left<=mid && right<=high)
+        {
+            if(nums[left]>2LL*nums[right])
+            {
+                count += mid-left+1;
+                right++;
+            }
+            else{
+                left++;
+            }
+
+        }
+        left =low;
+        right=mid+1;
+        while(left<=mid && right<=high)
+        {
+            if(nums[left]<=nums[right])
+            {
+                tmp.push_back(nums[left]);
+                left++;
+            }
+            else
+            {
+                tmp.push_back(nums[right]);
+                right++;
+            }
+
+        }
+        while(left<=mid)
+        {
+            tmp.push_back(nums[left]);
+                left++;
+        }
+        while(right<=high)
+        {
+            tmp.push_back(nums[right]);
+                right++;
+        }
+        for(int i=low;i<=high;i++)
+        {
+            nums[i]=tmp[i-low];
+        }
+        return count;
+
+    }
+};
