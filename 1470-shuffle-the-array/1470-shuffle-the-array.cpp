5a42bd2290c4a86bfa64569c4620f38c18df0378
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-24 12:50:22 +0530

    Time: 3 ms (72.72%), Space: 13.5 MB (17.42%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int>ans;
-        int lo =0;
-        int hi = n;
-        while(lo<n && hi<2*n)
-        {
-            ans.push_back(nums[lo]);
-            ans.push_back(nums[hi]);
-            lo++;
-            hi++;
-
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
         }
         return ans;
     }
