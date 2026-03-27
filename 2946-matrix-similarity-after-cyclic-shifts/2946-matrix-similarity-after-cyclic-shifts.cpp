183f757b8223374f74c1762b644d1806073f1866
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-27 10:44:05 +0530

    Time: 0 ms (100%), Space: 31.3 MB (67.7%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -4,7 +4,6 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         k = k%n;
-        vector<vector<int>>ans(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
