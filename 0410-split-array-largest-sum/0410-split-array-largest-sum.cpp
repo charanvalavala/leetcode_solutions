0ce7816a7e1554e8f81a4625ea1ada6cef7b517d
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-29 16:54:59 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.85%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -14,8 +14,19 @@ public:
         while(low<=high)
         {
             int mid = (low+high)/2;
-            int a = books(arr,mid);
-            if(a<=k)
+            int count=0;
+            int sum =0;
+            for(int i=0;i<n;i++)
+            {
+                sum += arr[i];
+                if(sum > mid)
+                {
+                    count++;
+                    sum =arr[i];
+                }
+            }   
+            if(sum !=0)count +=1;
+            if(count<=k)
             {
                 ans =mid;
                 high = mid-1;
@@ -26,22 +37,5 @@ public:
             }
         }
         return ans;
-    }
-        int books(vector<int> &arr,int mid)
-    {
-        int n = arr.size();
-        int count=0;
-        int sum =0;
-        for(int i=0;i<n;i++)
-        {
-            sum += arr[i];
-            if(sum > mid)
-            {
-                count++;
-                sum =arr[i];
-            }
-        }
-        if(sum !=0)count +=1;
-        return count;
     }
 };
