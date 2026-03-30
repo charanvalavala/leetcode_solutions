8d64096b11d1a7a96ada46bd4a439626bea26fc2
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-30 21:13:58 +0530

    Time: 4 ms (83.33%), Space: 17.7 MB (91.38%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    bool checkStrings(string s1, string s2) {
-        int n = s1.size();
-        vector<int>even(26,0);
-        vector<int>odd(26,0);
-        for(int i=0;i<n;i++)
-        {
-            if(i%2==0)
-            {
-                even[s1[i]-'a']++;
-                even[s2[i]-'a']--;
-            }
-            else
-            {
-                odd[s1[i]-'a']++;
-                odd[s2[i]-'a']--;
-            }
+    static bool checkStrings(string& s1, string& s2) {
+        array<array<int, 26>, 2> freq{};
+        constexpr array<array<int, 26>, 2> zero{};
+        const int n=s1.size();
+        for(int i=0; i<n; i++){
+            bool iOdd=i&1;
+            freq[iOdd][s1[i]-'a']++;
+            freq[iOdd][s2[i]-'a']--;
         }
-        for(int i =0;i<26;i++)
-        {
-            if(even[i] !=0 || odd[i] != 0)
-            {
-                return false;
-            }
-        }
-        return true;
-
+        return freq==zero;
     }
-    
 };
+
 auto init = []() {
     ios::sync_with_stdio(0);
     cin.tie(0);
