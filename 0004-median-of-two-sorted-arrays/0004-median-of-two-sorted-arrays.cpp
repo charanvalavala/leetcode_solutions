d3dc8ae58d29b4f8bfedf0493d3e3cf54f54dc88
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-03-31 19:37:52 +0530

    Time: 0 ms (100%), Space: 94.9 MB (94.34%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,44 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size()<nums1.size())return findMedianSortedArrays(nums2,nums1);
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int low =0;
-        int high = n1;
-        while(low<=high)
-        {
-            int cut1 = (low+high)/2;
-            int cut2 = (n1+n2+1)/2 - cut1;
-            int l1 = cut1 == 0?INT_MIN:nums1[cut1-1];
-            int l2 = cut2 == 0?INT_MIN:nums2[cut2-1];
+        
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
 
-            int h1 = cut1 == n1?INT_MAX:nums1[cut1];
-            int h2 = cut2 == n2?INT_MAX:nums2[cut2];
-            if(l1<=h2 && l2 <=h1)
-            {
-                if((n1+n2)%2==0)
-                {
-                    return (max(l1,l2)+min(h1,h2))/2.0;
-                }
-                else
-                {
-                    return max(l1,l2);
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int low = 0, high = m;
+
+        while (low <= high) {
+            int px = (low + high) / 2;
+            int py = (m + n + 1) / 2 - px;
+
+            int leftX  = (px == 0) ? INT_MIN : nums1[px - 1];
+            int rightX = (px == m) ? INT_MAX : nums1[px];
+
+            int leftY  = (py == 0) ? INT_MIN : nums2[py - 1];
+            int rightY = (py == n) ? INT_MAX : nums2[py];
+
+            if (leftX <= rightY && leftY <= rightX) {
+                if ((m + n) % 2 == 0) {
+                    return (max(leftX, leftY) + min(rightX, rightY)) / 2.0;
+                } else {
+                    return max(leftX, leftY);
                 }
             }
-            else if(l1>h2)
-            {
-                high = cut1-1;
+            else if (leftX > rightY) {
+                high = px - 1;
             }
-            else
-            {
-                low = cut1+1;
+            else {
+                low = px + 1;
             }
         }
+
         return 0.0;
     }
 };
