fa88d37422d155debf755c224a1d468b4d667d60
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-04-04 19:47:55 +0530

    Time: 0 ms (100%), Space: 11.2 MB (53.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char, int> mp;
-        for (auto c : s)
-            mp[c]++;
-
-        vector<pair<int, char>> v;
-        for (auto it : mp)
-            v.push_back({it.second, it.first});
-
-        sort(v.rbegin(), v.rend());
-
-        string ans = "";
-        for (auto it : v) {
-            while (it.first-- > 0)
-                ans.push_back(it.second);
+        unordered_map<char, int> freq;
+        for(char c : s) {
+            freq[c]++;
         }
+        vector<pair<int,char>> v;
+        for(auto it : freq) {
+            v.push_back({it.second, it.first});
+        }
+         sort(v.begin(), v.end(), greater<>());
+        string ans = "";
+        for(auto it : v) {
+            ans.append(it.first, it.second);
+        }
+
         return ans;
     }
 };
