0d7f2306fae79669ee4f0ad0bb5bf8d270ccd9c2
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-04-05 09:17:48 +0530

    Time: 4 ms (38.32%), Space: 10.9 MB (81.2%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        /*
-        unordered_map<char, int> freq;
-        int n = s.size();
-        for (char c : s)
-            freq[c]++;
-
-        vector<vector<char>> temp(n + 1);
-        for (auto it : freq)
-            temp[it.second].push_back(it.first);
-
-        string ans = "";
-        for (int i = n; i >= 1; i--)
-            for (char c : temp[i])
-                for (int j = 0; j < i; j++)
-                    ans.push_back(c);
-
-        return ans;
-        */
-        unordered_map<char, int> freq;
-        int n = s.size();
-        for (char c : s)
-            freq[c]++;
-        priority_queue<pair<int, char>> pq;
-        for (auto it : freq) {
-            pq.push({it.second, it.first});
+        /*int n=s.size();
+        map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[s[i]]++;
         }
-        string ans = "";
-        while (!pq.empty()) {
-            auto top = pq.top();
+       set<int,greater<int>>st;
+        for(auto it:mpp){
+           st.insert(it.second);
+        }
+        string ans="";
+        for(auto x:st){
+            for(auto it:mpp){
+                  if(it.second==x){
+                    for(int k=0;k<x;k++){
+                        ans+=it.first;
+                    }
+                }
+            }
+        }
+        return ans;*/
+        int n=s.size();
+        map<char,int>mpp;
+        for(char c:s){
+            mpp[c]++;
+        }
+        priority_queue<pair<int,char>>pq;
+        for(auto it:mpp){
+            pq.push({it.second,it.first});
+        }
+        string ans="";
+        while(!pq.empty()){
+            auto top=pq.top();
             pq.pop();
-            int count = top.first;
-            int item = top.second;
-            while (count-- > 0) {
-                ans.push_back(top.second);
+            int count=top.first;
+            char ch=top.second;
+            while(count--){
+                ans+=ch;
             }
         }
         return ans;
