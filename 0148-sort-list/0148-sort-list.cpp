872684246b3b70680d54bd19d031e72f03f80d29
Valavala Charan Teja <112560602+charanvalavala@users.noreply.github.com>
2026-04-11 21:40:41 +0530

    Time: 40 ms (39.88%), Space: 75.8 MB (6.45%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,22 +11,43 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        ListNode* tmp = head;
-        while(tmp != nullptr)
-        {
-            arr.push_back(tmp->val);
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* anoHead = slow->next;
+        slow->next = nullptr;
+
+        ListNode* head1 = sortList(head);
+        ListNode* head2 = sortList(anoHead);
+
+        return merge(head1, head2);
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* ans = new ListNode(0);
+        ListNode* tmp = ans;
+
+        while (head1 != nullptr && head2 != nullptr) {
+            if (head1->val <= head2->val) {
+                tmp->next = head1;
+                head1 = head1->next;
+            } else {
+                tmp->next = head2;
+                head2 = head2->next;
+            }
             tmp = tmp->next;
         }
-        sort(arr.begin(),arr.end());
-        tmp =head;
-        int n = arr.size();
-        for(int i=0;i<n;i++)
-        {
-            tmp->val = arr[i];
-            tmp=tmp->next;
-        }
-        return head;
-        
+
+        tmp->next = (head1 != nullptr) ? head1 : head2;
+
+        return ans->next;
     }
 };
